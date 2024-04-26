a212498a7e5139ebe8404f96a79eff1d66fbe79d
Amite55 <amite403@gmail.com>
2024-04-26 13:56:17 +0600

    added footer section

--- src/Pages/HostelDetails.jsx
+++ src/Pages/HostelDetails.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+
+import { Link, useLoaderData, useParams } from 'react-router-dom';
 
 const HostelDetails = () => {
     const hostels = useLoaderData();
-    const {id} = useParams();
+    const { id } = useParams();
     // console.log(hostels, id)
     const details = hostels.find(detail => detail.id == id);
     console.log(details)
     return (
-        <div>
-            <h1>hostel details</h1>
-            <img src={details.images} alt="not found" />
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img src={details.images} alt="Movie" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{details.segment_name}</h2>
+                <p>{details.description}</p>
+                <div className="card-actions justify-end">
+                   <Link to="/">
+                   <button className="btn btn-primary">Go Back</button>
+                   </Link>
+                </div>
+            </div>
         </div>
     );
 };
